4e55ea49cb17e17bc089c8efebd9580ef51fb1e2
BombenDev <nicolasbomben@gmail.com>
2022-03-29 19:29:17 -0300

    trabajando carrito de compras

--- js/carrito.js
+++ js/carrito.js
@@ -1,6 +1,7 @@
 class Producto{
 
-    constructor(nombre,precio,talle) {
+    constructor(id,nombre,precio,talle) {
+        this.id = id;
         this.nombre = nombre;
         this.precio = parseFloat(precio);
         this.talle = talle;
@@ -8,30 +9,22 @@ class Producto{
 
 }
 
+
 //declaro array vacio donde se van a ir guardando las cargas de productos.
 const productos = [];
 
-//uso metodo push para almacenar los productos que estan disponibles en la tienda.
-productos.push(new Producto("Remera Basica Negra", 2800, "M,L"));
-productos.push(new Producto("Morral 01", 1800, ""));
-productos.push(new Producto("Pantalon Jogger Estampado", 5000, "M,L,XL"));
-productos.push(new Producto("Remera Basica Blanca", 2800, "M,L"));
-productos.push(new Producto("Gorra Color Block", 1500, ""));
-productos.push(new Producto("Pantalon Soft Denim Acid", 4500, "M,L,XL"));
-
 console.log(productos);
 
-//let contador = 0;
+//uso metodo push para almacenar los productos que estan disponibles en la tienda.
+productos.push(new Producto(1,"Remera Basica Negra", 2800, "M,L"));
+productos.push(new Producto(2,"Morral 01", 1800, ""));
+productos.push(new Producto(3,"Pantalon Jogger Estampado", 5000, "M,L,XL"));
+productos.push(new Producto(4,"Remera Basica Blanca", 2800, "M,L"));
+productos.push(new Producto(5,"Gorra Color Block", 1500, ""));
+productos.push(new Producto(6,"Pantalon Soft Denim Acid", 4500, "M,L,XL"));
 
-/*window.addEventListener("load", () =>{
-    for(let i = 0; i < localStorage.length; i++){
-        let clave = localStorage.key(i);
-        let producto = localStorage.getItem(clave);
-        productos.push(JSON.parse(producto));
-        contador = i + 1;
-    }
-});*/
-//muestro los productos por HTML mediante manipulacion del DOM.
+
+//utilizo un for of para recorrer el array y DOM para visualizar en pantalla los productos.
     const containerProductos = document.getElementById("contenedor")
     for( let p of productos){
         let productoHtml = document.createElement("div")
@@ -46,10 +39,28 @@ console.log(productos);
                         <h3>${p.precio}</h3>
                         <h4>${p.talle}</h4>
                         <br>
-                        <button class="btn">AGREGAR</button>
+                        <button class="btn" id="btnAgregar">AGREGAR</button>
                       </div>
                   </div>
         `;
         containerProductos.appendChild(productoHtml);
+
+        
 }
 
+//declaro array de carrito donde se guardar los productos agregados.
+const carrito = [];
+
+//id Storage
+//let contador = 0;
+
+const botonAgregar = document.getElementById("btnAgregar");
+//funcion al cargar la pagina y local storage
+botonAgregar.addEventListener("click", (e) =>{
+    for(let i = 0; i < productos.length; i++){
+        carrito.push(e.target.productos);
+    }
+    //cargarListaProductos();
+});
+
+console.log(carrito);
